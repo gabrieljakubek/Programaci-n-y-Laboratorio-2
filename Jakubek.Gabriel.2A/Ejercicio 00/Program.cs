0a1bc11ae9170beec988659119e482a3e5ef696b
gabrieljakubek <gabrieljakubek@gmail.com>
2018-08-21 12:14:17 -0300

    Add files via upload

---
+++ Jakubek.Gabriel.2A/Ejercicio 00/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 00";
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            string nombre;
+            int edad;
+            Console.Write("Hola Mundo!!!");
+            Console.ReadLine();
+            Console.Write("Ingrese su Nombre:");
+            nombre = Console.ReadLine();
+            Console.Write("Su nombre es: {0}", nombre);
+            Console.ReadLine();
+            Console.Write("Ingrese su Edad:");
+            edad = int.Parse(Console.ReadLine());
+            Console.Write("Su edad es: {0}", edad);
+            Console.ReadLine();
+        }
+    }
+}
