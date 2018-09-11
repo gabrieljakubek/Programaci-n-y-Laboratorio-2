a3df0a1d9dee7d706e952ec0c5715d423b90ce7d
gabrieljakubek <gabrieljakubek@gmail.com>
2018-09-10 23:54:02 -0300

    Se realizo el ejercício 17

--- Jakubek.Gabriel.2A/Ejercicio 17/Boligrafo.cs
+++ Jakubek.Gabriel.2A/Ejercicio 17/Boligrafo.cs
@@ -4,10 +4,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ejercicio_17
+namespace Boligrafo
 {
     class Boligrafo
     {
+        #region Atributos
+        private readonly short cantidadTintaMaxima = 100;
+        private ConsoleColor _color;
+        private short _tinta;
+        #endregion
+
+        #region Constructores
+
+        public Boligrafo()
+        {
+            this._tinta = 100;
+            this._color = ConsoleColor.Black;
+        }
+
+        public Boligrafo(short tinta) : this()
+        {
+            this._tinta = tinta;
+        }
+
+        public Boligrafo(ConsoleColor color, short tinta) : this(tinta)
+        {
+            this._color = color;
+        }
+        #endregion
+
+        #region Metodos
+        public ConsoleColor GetColor()
+        {
+            return this._color;
+        }
+
+        public short GetTinta()
+        {
+            return this._tinta;
+        }
+
+        public bool Pintar(int gasto, out string dibujo)
+        {
+            bool retorno = false;
+            int i = 0;
+            dibujo = "";
+            if (this._tinta > 0)
+            {
+                if (gasto > this._tinta)
+                {
+                    for (i = 0; i < this._tinta; i++)
+                    {
+                        dibujo += "*";
+                    }
+                    this._tinta = 0;
+                }
+                else
+                {
+                    for (i = 0; i < gasto; i++)
+                    {
+                        dibujo += "*";
+                    }
+                    this._tinta -= (short)gasto;
+                }
+                Console.ForegroundColor = this._color;
+                Console.WriteLine(dibujo);
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+            }
+            else
+            {
+                Console.WriteLine("Recargue la tinta del boligrafo!!!");
+            }
+            return retorno;
+        }
+
+        public void Recarga()
+        {
+            this.SetTinta(this._tinta);
+        }
+
+        private void SetTinta(short tinta)
+        {
+            if (tinta < 100)
+            {
+                this._tinta = 100;
+            }
+        }
+        #endregion
 
     }
 }
--- Jakubek.Gabriel.2A/Ejercicio 17/Program.cs
+++ Jakubek.Gabriel.2A/Ejercicio 17/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Boligrafo;
 
 namespace Ejercicio_17
 {
@@ -10,6 +11,41 @@ namespace Ejercicio_17
     {
         static void Main(string[] args)
         {
+            Console.Title = "Ejercicio 17";
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            ConsoleColor color;
+            short tinta;
+            int gastar = 0;
+            string chequeo;
+            bool comprobacion = false;
+
+            color = ConsoleColor.Blue;
+            tinta = 100;
+            Boligrafo.Boligrafo boligrafo1 = new Boligrafo.Boligrafo(color, tinta);
+
+            Console.WriteLine("El color del boligrafo es {0} y tiene {1} de tinta", boligrafo1.GetColor(), boligrafo1.GetTinta());
+
+            color = ConsoleColor.Red;
+            tinta = 50;
+            Boligrafo.Boligrafo boligrafo2 = new Boligrafo.Boligrafo(color, tinta);
+
+            Console.WriteLine("El color del boligrafo es {0} y tiene {1} de tinta", boligrafo2.GetColor(), boligrafo2.GetTinta());
+
+            gastar = 110;
+            comprobacion = boligrafo1.Pintar(gastar, out chequeo);
+            Console.WriteLine("El color del boligrafo es {0} y tiene {1} de tinta", boligrafo1.GetColor(), boligrafo1.GetTinta());
+
+            boligrafo1.Recarga();
+            Console.WriteLine("El color del boligrafo es {0} y tiene {1} de tinta", boligrafo1.GetColor(), boligrafo1.GetTinta());
+
+            comprobacion = boligrafo2.Pintar(gastar, out chequeo);
+            Console.WriteLine("El color del boligrafo es {0} y tiene {1} de tinta", boligrafo2.GetColor(), boligrafo2.GetTinta());
+
+            boligrafo2.Recarga();
+            Console.WriteLine("El color del boligrafo es {0} y tiene {1} de tinta", boligrafo2.GetColor(), boligrafo2.GetTinta());
+
+            Console.ReadLine();
         }
     }
 }
